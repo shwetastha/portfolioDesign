417bb626320ae82b10e4cd2ca54f3e969e76250a
shwetastha <shweta.stha1@gmail.com>
2019-02-25 20:37:10 -0500

    Directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,10 @@ import { ProjectComponent } from './project/project.component';
 
 import { MatListModule } from '@angular/material/list';
 
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -23,7 +27,10 @@ import { MatListModule } from '@angular/material/list';
     BrowserAnimationsModule,
     MatToolbarModule,
     FlexLayoutModule,
-    MatListModule
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,6 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { Portfolio } from '../shared/portfolio';
 
+const PORTFOLIOS: Portfolio[] = [
+  {
+    id: '0',
+    name: 'Sales Traker',
+    image: '/assets/imgaes/SalesTracker.png',
+    category: 'mains',
+    featured: true,
+    label: 'Hot',
+    // tslint:disable-next-line:max-line-length
+    description: 'This is an android application designed specifically to send information to a server through internet access. This application sends Imei no, Model no, Operator and Country_iso of android smart phone to server, which uses the following url "http://xxx.xxx.xxx.xx/sales_tracker/insert.php". It reads dumpsys file to retrieve IMEI information.'
+  },
+  {
+    id: '1',
+    name: 'Premium SMS',
+    image: '/assets/imgaes/Premium_SMS.png',
+    category: 'appetizer',
+    featured: false,
+    label: '',
+    description: 'This android application send SMS to subscribe and unsubscribe various services provided by the network. This application was made specifically for MediaTek android devices, using the MediaTek Inc. SDK [MediaTek Add-On:19].'
+  }
+ ];
+
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
@@ -8,27 +30,8 @@ import { Portfolio } from '../shared/portfolio';
 })
 export class ProjectComponent implements OnInit {
 
-  portfolios: Portfolio[] = [
-    {
-      id: '0',
-      name: 'Sales Traker',
-      image: '/assets/imgaes/SalesTracker.png',
-      category: 'mains',
-      featured: true,
-      label: 'Hot',
-      // tslint:disable-next-line:max-line-length
-      description: 'This is an android application designed specifically to send information to a server through internet access. This application sends Imei no, Model no, Operator and Country_iso of android smart phone to server, which uses the following url "http://xxx.xxx.xxx.xx/sales_tracker/insert.php". It reads dumpsys file to retrieve IMEI information.'
-    },
-    {
-      id: '1',
-      name: 'Premium SMS',
-      image: '/assets/imgaes/Premium_SMS.png',
-      category: 'appetizer',
-      featured: false,
-      label: '',
-      description: 'This android application send SMS to subscribe and unsubscribe various services provided by the network. This application was made specifically for MediaTek android devices, using the MediaTek Inc. SDK [MediaTek Add-On:19].'
-    }
-   ];
+  portfolios= PORTFOLIOS;
+  selectedPortfolio: Portfolio = PORTFOLIOS[0];
 
   constructor() { }
 
